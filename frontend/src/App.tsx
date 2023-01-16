1d1190e44d66abc750e74c3a4e5952b70a345bed
Shubham Jain <shubham@sjain.dev>
2023-01-16 19:03:50 +0000

    Create initial frontend structure
    This will likely change quite a bit once we have our auth and API
    gateway plan finalised (e.g., using Next.js or not), but the React
    components etc. that we write will be able to be copied over.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Forum from "./Forum";
+
+const App = () => {
+  return (
+    <>
+      <nav>
+        <h1>OXFAM Project</h1>
+      </nav>
+      <main>
+        <Forum />
+      </main>
+    </>
+  );
+};
+
+export default App;
