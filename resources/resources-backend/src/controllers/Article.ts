18ac0d9c4e59838fb3abb8378dec857e4703aa0a
Adnan Benachar <adnanbenachar@Adnans-MacBook-Air.local>
2023-01-20 11:13:19 +0000

    Add more CRUD

--- resources/resources-backend/src/controllers/Article.ts
+++ resources/resources-backend/src/controllers/Article.ts
@@ -19,10 +19,52 @@ const createArticle = (req: Request, res: Response, next: NextFunction) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
+const getArticle = (req: Request, res: Response, next: NextFunction) => {
+  const articleId = req.params.articleId
+
+  return Article.findById(articleId)
+    .then((article) => (article ? res.status(200).json({article}) : res.status(404).json({message: 'Article not found'})))
+    .catch((error) => res.status(500).json({ error }));
+    
+}
+
 const getAll = (req: Request, res: Response, next: NextFunction) => {
   return Article.find()
     .then((articles) => res.status(201).json({ articles }))
     .catch((error) => res.status(500).json({ error }));
 };
 
-export default { createArticle, getAll };
+const updateArticle = (req: Request, res: Response, next: NextFunction) => {
+  const articleId = req.params.articleId
+
+  return Article.findById(articleId)
+    .then((article) => 
+    {
+      if (article)
+      {
+        article.set(req.body)
+
+        return article
+        .save()
+        .then((article) => res.status(201).json({ article }))
+        .catch((error) => res.status(500).json({ error }));
+      }
+      else
+      {
+        res.status(404).json({message: 'Article not found'})
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
+}
+
+const deleteArticle = (req: Request, res: Response, next: NextFunction) => {
+  const articleId = req.params.articleId
+
+  return Article.findByIdAndDelete(articleId)
+    .then((article) => (article ? res.status(201).json({message: 'Article Deleted'}) : res.status(404).json({message: 'Article not found'})))
+    .catch((error) => res.status(500).json({ error }));
+    
+}
+
+
+export default { createArticle, getArticle, getAll, updateArticle, deleteArticle };
--- resources/resources-backend/src/routes/Article.ts
+++ resources/resources-backend/src/routes/Article.ts
@@ -4,6 +4,11 @@ import controller from "../controllers/Article";
 const router = express.Router();
 
 router.post("/create", controller.createArticle);
+
+router.get("/get/:articleId", controller.getArticle);
 router.get("/getall", controller.getAll);
+
+router.patch("/update/:articleId", controller.updateArticle);
+router.delete("/delete/:articleId", controller.deleteArticle);
 
 export = router;
