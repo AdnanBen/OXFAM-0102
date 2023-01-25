64df1c2e16136252a55f91485a191ac751595f65
adnanben <adnanb1112@gmail.com>
2023-01-25 20:37:25 +0000

    Added endpoints

--- resources/resources-backend/src/controllers/Article.ts
+++ resources/resources-backend/src/controllers/Article.ts
@@ -20,13 +20,16 @@ const createArticle = (req: Request, res: Response, next: NextFunction) => {
 };
 
 const getArticle = (req: Request, res: Response, next: NextFunction) => {
-  const articleId = req.params.articleId
+  const articleId = req.params.articleId;
 
   return Article.findById(articleId)
-    .then((article) => (article ? res.status(200).json({article}) : res.status(404).json({message: 'Article not found'})))
+    .then((article) =>
+      article
+        ? res.status(200).json({ article })
+        : res.status(404).json({ message: "Article not found" })
+    )
     .catch((error) => res.status(500).json({ error }));
-    
-}
+};
 
 const getAll = (req: Request, res: Response, next: NextFunction) => {
   return Article.find()
@@ -34,37 +37,58 @@ const getAll = (req: Request, res: Response, next: NextFunction) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
+const getAllTitles = (req: Request, res: Response, next: NextFunction) => {
+  return Article.find({}, "_id, title")
+    .then((articles) => res.status(201).json({ articles }))
+    .catch((error) => res.status(500).json({ error }));
+};
+
+const getByCategory = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.query);
+  const categoryParam = req.query?.category;
+
+  return Article.find({ category: categoryParam })
+    .then((articles) => res.status(201).json({ articles }))
+    .catch((error) => res.status(500).json({ error }));
+};
+
 const updateArticle = (req: Request, res: Response, next: NextFunction) => {
-  const articleId = req.params.articleId
+  const articleId = req.params.articleId;
 
   return Article.findById(articleId)
-    .then((article) => 
-    {
-      if (article)
-      {
-        article.set(req.body)
+    .then((article) => {
+      if (article) {
+        article.set(req.body);
 
         return article
-        .save()
-        .then((article) => res.status(201).json({ article }))
-        .catch((error) => res.status(500).json({ error }));
-      }
-      else
-      {
-        res.status(404).json({message: 'Article not found'})
+          .save()
+          .then((article) => res.status(201).json({ article }))
+          .catch((error) => res.status(500).json({ error }));
+      } else {
+        res.status(404).json({ message: "Article not found" });
       }
     })
     .catch((error) => res.status(500).json({ error }));
-}
+};
 
 const deleteArticle = (req: Request, res: Response, next: NextFunction) => {
-  const articleId = req.params.articleId
+  const articleId = req.params.articleId;
 
   return Article.findByIdAndDelete(articleId)
-    .then((article) => (article ? res.status(201).json({message: 'Article Deleted'}) : res.status(404).json({message: 'Article not found'})))
+    .then((article) =>
+      article
+        ? res.status(201).json({ message: "Article Deleted" })
+        : res.status(404).json({ message: "Article not found" })
+    )
     .catch((error) => res.status(500).json({ error }));
-    
-}
+};
 
-
-export default { createArticle, getArticle, getAll, updateArticle, deleteArticle };
+export default {
+  createArticle,
+  getArticle,
+  getAll,
+  getAllTitles,
+  getByCategory,
+  updateArticle,
+  deleteArticle,
+};
--- resources/resources-backend/src/routes/Article.ts
+++ resources/resources-backend/src/routes/Article.ts
@@ -8,6 +8,9 @@ router.post("/create", controller.createArticle);
 router.get("/get/:articleId", controller.getArticle);
 router.get("/getall", controller.getAll);
 
+router.get("/getalltitles", controller.getAllTitles);
+router.get("/getbycategory", controller.getByCategory);
+
 router.patch("/update/:articleId", controller.updateArticle);
 router.delete("/delete/:articleId", controller.deleteArticle);
 
