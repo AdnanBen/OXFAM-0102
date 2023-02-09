fa5c4c3106dfaaf50282bc296f1d14368d81ea25
Shubham Jain <shubham@sjain.dev>
2023-02-09 20:17:40 +0000

    Add docker-compose-prod.yml's

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-docker-compose -f forum/docker-compose.yml -f gateway/docker-compose.yml -f moderator-frontend/docker-compose.yml -f reports/docker-compose.yml -f resources/docker-compose.yml -f survivor-frontend/docker-compose.yml up
+docker-compose -f forum/docker-compose-prod.yml \
+  -f gateway/docker-compose-prod.yml \
+  # -f moderator-frontend/docker-compose.yml \
+  -f reports/docker-compose-prod.yml \
+  -f resources/docker-compose-prod.yml \
+  -f survivor-frontend/docker-compose-prod.yml \
+  up -d
