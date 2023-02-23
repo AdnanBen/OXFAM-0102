c1a6697a3fd98c40d3c489950fe04e62c41e5d53
Shubham Jain <shubham@sjain.dev>
2023-02-23 20:49:41 +0000

    Update env schema & fix server-side validation
    The server-side validation was previously running on the client when
    NextAuth.js authOptions was imported into a frontend component.

--- survivor-frontend/src/env/server.mjs
+++ survivor-frontend/src/env/server.mjs
@@ -7,21 +7,21 @@ import { serverSchema, serverEnv } from "./schema.mjs";
 import { env as clientEnv, formatErrors } from "./client.mjs";
 
 const _serverEnv = serverSchema.safeParse(serverEnv);
+if (typeof window === "undefined") {
+  if (!_serverEnv.success) {
+    console.error(
+      "❌ Invalid environment variables:\n",
+      ...formatErrors(_serverEnv.error.format())
+    );
+    throw new Error("Invalid environment variables");
+  }
 
-if (!_serverEnv.success) {
-  console.error(
-    "❌ Invalid environment variables:\n",
-    ...formatErrors(_serverEnv.error.format()),
-  );
-  throw new Error("Invalid environment variables");
-}
+  for (let key of Object.keys(_serverEnv.data)) {
+    if (key.startsWith("NEXT_PUBLIC_")) {
+      console.warn("❌ You are exposing a server-side env-variable:", key);
 
-for (let key of Object.keys(_serverEnv.data)) {
-  if (key.startsWith("NEXT_PUBLIC_")) {
-    console.warn("❌ You are exposing a server-side env-variable:", key);
-
-    throw new Error("You are exposing a server-side env-variable");
+      throw new Error("You are exposing a server-side env-variable");
+    }
   }
 }
-
-export const env = { ..._serverEnv.data, ...clientEnv };
+export const env = { ...(_serverEnv?.data ?? {}), ...clientEnv };
