5b717e977d7b3fa04c5612f1f1a2b9dd3ca4748b
lillyzha <zcabqzh@ucl.ac.uk>
2023-02-23 17:56:33 +0000

    added board

--- survivor-frontend/src/pages/forum/index.tsx
+++ survivor-frontend/src/pages/forum/index.tsx
@@ -46,10 +46,21 @@ const Feed: NextPage = () => {
 
       <main>
         <h2>Forum</h2>
+        <h3>Posts</h3>
         {posts?.posts?.map((p) => (
           <Post post={p} />
         ))}
         <Link href="/forum/new">Add new post</Link>
+        <br />
+        <h3>Boards</h3>
+        <text>We have oragnised the posts according to the boards to make it easier for you to find support, information and talk to others with similar experiences.</text>
+        <br />
+        <input className={styles.board_search} type="text" placeholder="Search.." /> 
+        <br />
+        <button className={styles.board_button}>General discussion</button> 
+        <button className={styles.board_button}>Requests for advice</button>
+        <button className={styles.board_button}>Tell me about yourself</button> 
+        <button className={styles.board_button}>Political discussions</button>
       </main>
     </>
   );
