291c8d98700dc85800ca5809e60a1645010dc294
Shubham Jain <shubham@sjain.dev>
2023-03-02 18:52:21 +0000

    Improve homepage

--- survivor-frontend/src/pages/index.tsx
+++ survivor-frontend/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { Trans } from "@lingui/macro";
 import { type NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
+import { Panel } from "rsuite";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -12,13 +14,82 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className={styles.container}>
         <p className={styles.intro}>
           <Trans>
             A safe-space for survivors to discuss and share their experiences of
             abuse, and get support.
           </Trans>
         </p>
+
+        <div className={styles.links}>
+          <Link href="/forum">
+            <Panel
+              header={
+                <span className={styles.linkHeader}>
+                  <Trans>Forum</Trans> &rarr;
+                </span>
+              }
+              bordered
+              shaded
+              style={{ width: "100%" }}
+            >
+              <Trans>
+                Safely discuss your concerns and receive feedback from the
+                community.
+              </Trans>
+            </Panel>
+          </Link>
+          <Link href="/chat">
+            <Panel
+              header={
+                <span className={styles.linkHeader}>
+                  <Trans>Chat</Trans> &rarr;
+                </span>
+              }
+              bordered
+              shaded
+              style={{ width: "100%" }}
+            >
+              <Trans>
+                Chat individually with trained moderators for advice.
+              </Trans>
+            </Panel>
+          </Link>
+          <Link href="/resources">
+            <Panel
+              header={
+                <span className={styles.linkHeader}>
+                  <Trans>Resources</Trans> &rarr;
+                </span>
+              }
+              bordered
+              shaded
+              style={{ width: "100%" }}
+            >
+              <Trans>
+                Find free self-help and educational resources to help manage and
+                recover from abuse.
+              </Trans>
+            </Panel>
+          </Link>
+          <Link href="/report">
+            <Panel
+              header={
+                <span className={styles.linkHeader}>
+                  <Trans>Report</Trans> &rarr;
+                </span>
+              }
+              bordered
+              shaded
+              style={{ width: "100%" }}
+            >
+              <Trans>
+                Report severe abuse to receive help from trained responders.
+              </Trans>
+            </Panel>
+          </Link>
+        </div>
       </main>
     </>
   );
