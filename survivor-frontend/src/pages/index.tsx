4e611d5dadacf655f8f1ccc8ef5f62878b359160
Jaruphop  Pataraanant <jaruphoppataraanant@Jaruphops-MacBook-Pro.local>
2023-03-09 18:01:17 +0000

    removed navigation bar and optimised the frontend for mobile

--- survivor-frontend/src/pages/index.tsx
+++ survivor-frontend/src/pages/index.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { Panel } from "rsuite";
 import styles from "../styles/Home.module.css";
 
+
 const Home: NextPage = () => {
   return (
     <>
@@ -15,6 +16,8 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.container}>
+
+      
         <p className={styles.intro}>
           <Trans>
             A safe-space for survivors to discuss and share their experiences of
@@ -23,70 +26,85 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.links}>
-          <Link href="/forum">
-            <Panel
-              header={
-                <span className={styles.linkHeader}>
-                  <Trans>Forum</Trans> &rarr;
-                </span>
-              }
-              bordered
-              shaded
-              style={{ width: "100%" }}
-            >
+        <Link href="/forum">
+        <Panel
+          header={
+            <span className={styles.linkHeader}>
               <Trans>
-                Safely discuss your concerns and receive feedback from the
-                community.
+                Forum <span className="link-text">&rarr;</span> 
               </Trans>
-            </Panel>
-          </Link>
+            </span>
+            
+          }
+
+          bordered
+          shaded
+          style={{ width: "100%" }}
+        >
+           <span className="link-text">
+            <Trans>
+              Safely discuss your concerns and receive feedback from the community.
+            </Trans>
+          </span>
+
+         </Panel>
+        </Link>
+        
+          
+        
           <Link href="/chat">
             <Panel
               header={
                 <span className={styles.linkHeader}>
-                  <Trans>Chat</Trans> &rarr;
+                  Chat <span className="link-text">&rarr;</span>  
                 </span>
               }
               bordered
               shaded
               style={{ width: "100%" }}
             >
+              <span className="link-text">
               <Trans>
                 Chat individually with trained moderators for advice.
               </Trans>
+              </span>
             </Panel>
           </Link>
           <Link href="/resources">
             <Panel
               header={
                 <span className={styles.linkHeader}>
-                  <Trans>Resources</Trans> &rarr;
+                  Resources <span className="link-text">&rarr;</span> 
                 </span>
               }
               bordered
               shaded
               style={{ width: "100%" }}
             >
+              <span className="link-text">
               <Trans>
                 Find free self-help and educational resources to help manage and
                 recover from abuse.
               </Trans>
+              </span>
             </Panel>
           </Link>
           <Link href="/report">
             <Panel
               header={
                 <span className={styles.linkHeader}>
-                  <Trans>Report</Trans> &rarr;
+                  Report <span className="link-text">&rarr;</span> 
                 </span>
               }
               bordered
               shaded
               style={{ width: "100%" }}
             >
+              <span className="link-text">
               <Trans>
                 Report severe abuse to receive help from trained responders.
               </Trans>
+              </span>
             </Panel>
           </Link>
         </div>
