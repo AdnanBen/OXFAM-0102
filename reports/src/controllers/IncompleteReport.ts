c38f1a22267eb7c89c9275c801997aaa8e7a829c
Shubham Jain <shubham@sjain.dev>
2023-03-13 21:52:00 +0000

    Move RabbitMQ hostname to env variable

--- reports/src/controllers/IncompleteReport.ts
+++ reports/src/controllers/IncompleteReport.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import amqplib from "amqplib";
 
 const publishOnQueue = async (queueName: string, msg: string) => {
-  const connection = await amqplib.connect("amqp://localhost");
+  const connection = await amqplib.connect(`amqp://${process.env.RABBITMQ_HOSTNAME}`);
   const channel = await connection.createChannel();
   await channel.assertQueue(queueName, { durable: false });
   channel.sendToQueue(queueName, Buffer.from(msg));
