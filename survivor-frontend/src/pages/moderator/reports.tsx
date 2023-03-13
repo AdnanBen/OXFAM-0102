5d059d54f8bd7cf4e807c0286eb88b27e2371952
AdnanBen <adnanb1112@gmail.com>
2023-03-13 17:29:29 +0000

    Remove incomp reports from reports moderator view (moved to trends)

--- survivor-frontend/src/pages/moderator/reports.tsx
+++ survivor-frontend/src/pages/moderator/reports.tsx
@@ -8,25 +8,16 @@ export const getServerSideProps: GetServerSideProps = (context) =>
 
 export default function ReportsView() {
   const [reports, setReports] = useState({});
-  const [incompleteReports, setIncompleteReports] = useState({});
 
   useEffect(() => {
     fetch("/api/moderators/reports/completereports/getall")
       .then((response) => response.json())
       .then((json) => setReports(json));
-
-    fetch("/api/moderators/reports/incompletereports/getall")
-      .then((response) => response.json())
-      .then((json) => setIncompleteReports(json));
   }, []);
 
   useEffect(() => {
     console.log(reports);
   }, [reports]);
-
-  useEffect(() => {
-    console.log(incompleteReports);
-  }, [incompleteReports]);
 
   return (
     <main className={styles.reportsmodpage}>
@@ -57,30 +48,6 @@ export default function ReportsView() {
       </div>
 
       <br />
-
-      <h3>Incomplete Reports</h3>
-
-      <div>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th className="text-center">Report ID</th>
-              <th className="text-center">Info</th>
-            </tr>
-          </thead>
-          <tbody>
-            {incompleteReports?.incompleteReports &&
-              incompleteReports.incompleteReports.map((x) => {
-                return (
-                  <tr>
-                    <td className="text-center">{x._id}</td>
-                    <td className="text-center">{x.info}</td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-      </div>
     </main>
   );
 }
