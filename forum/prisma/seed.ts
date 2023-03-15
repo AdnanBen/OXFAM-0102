f6cceb3ba97078b3950ca013fe49ebaa001ab587
Shubham Jain <shubham@sjain.dev>
2023-03-15 14:58:24 +0000

    Run forum seed script always but only add boards if none exist

--- forum/prisma/seed.ts
+++ forum/prisma/seed.ts
@@ -2,15 +2,20 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.board.createMany({
-    data: [
-      { name: "General Discussion", description: "General discussion topics" },
-      {
-        name: "Requests for Advice",
-        description: "Get advice about dealing with abuse",
-      },
-    ],
-  });
+  if ((await prisma.board.count()) === 0) {
+    await prisma.board.createMany({
+      data: [
+        {
+          name: "General Discussion",
+          description: "General discussion topics",
+        },
+        {
+          name: "Requests for Advice",
+          description: "Get advice about dealing with abuse",
+        },
+      ],
+    });
+  }
 }
 
 main()
