c2c484e4ad2f6904dba05def6ace9608cc2f51b4
Adnan Benachar <adnanbenachar@Adnans-MacBook-Air.local>
2023-03-23 09:32:24 +0000

    Add view accumulation logic

--- trends/src/controllers/ResourceView.ts
+++ trends/src/controllers/ResourceView.ts
@@ -5,7 +5,7 @@ import ResourceView from "../models/ResourceView";
 const createResourceView = (body: string) => {
   const bodyObj = JSON.parse(body);
 
-  const { resourceId } = bodyObj;
+  const { resourceId, timestamp } = bodyObj;
 
   const resourceView = new ResourceView({
     _id: new mongoose.Types.ObjectId(),
@@ -21,7 +21,57 @@ const getAll = (req: Request, res: Response, next: NextFunction) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
+const incrementCount = (
+  data: { [key: string]: number },
+  resourceId: string
+) => {
+  if (!data[resourceId]) {
+    data[resourceId] = 0;
+  }
+  data[resourceId] += 1;
+};
+
+const getTrends = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const now = new Date();
+    const oneDayAgo = new Date(now.getTime() - 24 * 60 * 60 * 1000);
+    const oneWeekAgo = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+    const allTimeTrends = await ResourceView.find();
+    const weeklyTrends = await ResourceView.find({
+      timestamp: { $gte: oneWeekAgo },
+    });
+    const dailyTrends = await ResourceView.find({
+      timestamp: { $gte: oneDayAgo },
+    });
+
+    const allTimeData: { [key: string]: number } = {};
+    const weeklyData: { [key: string]: number } = {};
+    const dailyData: { [key: string]: number } = {};
+
+    allTimeTrends.forEach((element) => {
+      incrementCount(allTimeData, element.resourceId);
+    });
+
+    weeklyTrends.forEach((element) => {
+      incrementCount(weeklyData, element.resourceId);
+    });
+
+    dailyTrends.forEach((element) => {
+      incrementCount(dailyData, element.resourceId);
+    });
+
+    return res.status(201).json({
+      allTime: allTimeData,
+      weekly: weeklyData,
+      daily: dailyData,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export default {
   createResourceView,
-  getAll,
+  getTrends,
 };
