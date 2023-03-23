bfe28a2172b82c779fb83485cd9e1b32ef521a5f
Adnan Benachar <adnanbenachar@Adnans-MacBook-Air.local>
2023-03-23 09:33:11 +0000

    Change route

--- trends/src/routes/ResourceView.ts
+++ trends/src/routes/ResourceView.ts
@@ -4,5 +4,5 @@ import controller from "../controllers/ResourceView";
 const router = express.Router();
 
 router.post("/", controller.createResourceView);
-router.get("/", controller.getAll);
+router.get("/", controller.getTrends);
 export default router;
