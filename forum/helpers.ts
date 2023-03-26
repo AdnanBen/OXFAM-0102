9d08313d29444fc9403fc2220b8b86893efc62b9
Shubham Jain <shubham@sjain.dev>
2023-03-27 00:01:57 +0100

    Only expect CAPTCHA in prod

--- forum/helpers.ts
+++ forum/helpers.ts
@@ -9,26 +9,29 @@ export class APIError extends Error {
   }
 }
 
-export async function requirecaptcha (req) {
-  if (!req.body.validated) {
-    const endpoint = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
-    const secret = process.env.CLOUDFLARE_SECRET
-  
+export async function requirecaptcha(req) {
+  if (!req.body.validated && process.env.NODE_ENV === "production") {
+    const endpoint =
+      "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+    const secret = process.env.CLOUDFLARE_SECRET;
+
     const resp = await fetch(endpoint, {
-      method: 'POST',
-      body: `secret=${encodeURIComponent(secret!)}&response=${encodeURIComponent(req.body.cftoken)}`,
+      method: "POST",
+      body: `secret=${encodeURIComponent(
+        secret!
+      )}&response=${encodeURIComponent(req.body.cftoken)}`,
       headers: {
-        'content-type': 'application/x-www-form-urlencoded'
-      }
-    })
-    const data = await resp.json()
+        "content-type": "application/x-www-form-urlencoded",
+      },
+    });
+    const data = await resp.json();
     console.log(req.body.cftoken);
     console.log(resp);
     console.log(data);
     if (!data.success) {
-      throw new APIError(401, "Captcha verification failed")
+      throw new APIError(401, "Captcha verification failed");
     }
-  
+
     return true;
   }
   return true;
